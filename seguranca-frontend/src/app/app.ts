6ac404a0dfa94e902a1b8d8b30e97c021dd1e757
CLARA ARAGAO PORTES <clara.portes@aluno.senac.rio>
2026-07-15 21:07:17 -0300

    15072026

---
+++ seguranca-frontend/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router'
+import { EpiList } from './epi/epi-list/epi-list';
+import { EpiDetalhe } from './epi/epi-detalhe/epi-detalhe';
+ 
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, EpiList, EpiDetalhe, RouterModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+ titulo = "Sistema de controle de EPI;";
+ subtitulo = "Gestão de segurança do trabalho";
+ descricao = "Aplicação para controle, monitoramento e conformidade dos equipamentos de proteção individual";
+  }
+  
+
